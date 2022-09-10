d9ede3fffa0000db1b5b320393dccea34a6c70ac
MosheCaustill <111441912+MosheCaustill@users.noreply.github.com>
2022-09-10 22:24:49 +0300

    auto restart

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-let num1;
-let num2;
-let result;
-let action="calc action"
-num1 = (prompt(`אנא הזן מספר`));
-if (Number(num1) == num1) {
-    num1=Number(num1);
-    num2 = (prompt(`אנא הזן מספר`));
+claculator();
+function claculator() {
+  let num1;
+  let num2;
+  let result;
+  let action = "calc action";
+  num1 = prompt(`אנא הזן מספר`);
+  if (Number(num1) == num1) {
+    num1 = Number(num1);
+    num2 = prompt(`אנא הזן מספר`);
     if (Number(num2) == num2) {
-        num2=Number(num2);
-        action = prompt(`אנא בחר פעולה *,/,+,-`);
-        if (action == `*`) {
-            result=num1*num2;
-        } else if (action == `/`) {
-            if (num2==0) {
-                result=`divide eror`;
-            } else {
-                result=num1/num2;
-            }
-        } else if (action == `+`) {
-            result=num1+num2;
-        } else if (action == `-`) {
-            result=num1-num2;
+      num2 = Number(num2);
+      action = prompt(`אנא בחר פעולה *,/,+,-`);
+      if (action == `*`) {
+        result = num1 * num2;
+      } else if (action == `/`) {
+        if (num2 == 0) {
+          alert(`divide eror`);
+          claculator();
         } else {
-            result=`Invalid Operator`
+          result = num1 / num2;
         }
-        alert(String(num1)+action+String(num2)+`=`+String(result));
+      } else if (action == `+`) {
+        result = num1 + num2;
+      } else if (action == `-`) {
+        result = num1 - num2;
+      } else {
+        alert(`Invalid Operator`);
+        claculator();
+      }
+      alert(String(num1) + action + String(num2) + `=` + String(result));
     } else {
-        alert(`Type Error`)
-        num2=`Type Error`;
+      alert(`Type Error`);
+      claculator();
     }
-} else {
-    alert(`Type Error`)
-    num1=`Type Error`;
+  } else {
+    alert(`Type Error`);
+    claculator();
+  }
 }
